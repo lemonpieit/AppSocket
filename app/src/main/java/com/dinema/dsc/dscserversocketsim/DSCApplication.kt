db7a5fba9b30bd6c90929aab32bfe1c5d631bc3d
simone <simone.mapelli@abenergie.it>
2024-03-26 10:12:20 +0100

    feat: sending async msg in random order ok, polish code

--- app/src/main/java/com/dinema/dsc/dscserversocketsim/DSCApplication.kt
+++ app/src/main/java/com/dinema/dsc/dscserversocketsim/DSCApplication.kt
@@ -14,6 +14,7 @@ import java.io.IOException
 import java.io.PrintWriter
 import java.net.ServerSocket
 import java.net.Socket
+import kotlin.random.Random
 
 
 class DSCApplication : MultiDexApplication() {
@@ -50,9 +51,7 @@ class DSCApplication : MultiDexApplication() {
         }
 
         fun startServerSocket() {
-
             println("Server in ascolto sulla porta $serverPort...")
-
             CoroutineScope(Dispatchers.IO).launch {
                 while (true) {
                     try {
@@ -60,17 +59,10 @@ class DSCApplication : MultiDexApplication() {
                         if (clientSocket != null) {
                             println("Connessione accettata da ${clientSocket!!.inetAddress.hostAddress}")
                             launch {
-                                /*handleClient(clientSocket!!,
-                                    "Your initial command here or modify as needed: ${count++}")*/
                                 handleClient_01(
-                                    clientSocket!!, "Initial command : TEST"
+                                    clientSocket!!, "Initial command : TEST "
                                 )
                             }
-                            // Gestisci la connessione client
-                            /*CoroutineScope(Dispatchers.IO).launch {
-                            // handleClient(clientSocket!!)
-                            handleClient()
-                        }*/
                         }
                     } catch (e: IOException) {
                         e.printStackTrace()
@@ -194,7 +186,7 @@ class DSCApplication : MultiDexApplication() {
                     val input = socket.getInputStream().bufferedReader()
                     val message = input.readLine()
                     println("Messaggio  ricevuto dal client :${message}")
-                    // open stream to sent msgs to client
+                    // open stream to send msgs to client
                     val output = PrintWriter(socket.getOutputStream(), true)
                     var count = 0
 
@@ -202,21 +194,20 @@ class DSCApplication : MultiDexApplication() {
                     output.println(initialCommand)
                     println("Initial command sent to client: $initialCommand")
 
-                    // sent commands seam to test client processing capability
                     try {
                         while (true) {
-                            val sendsCount = "Command count: ${count++}"
-                            println("Command count sent to client: $sendsCount")
+                            val sendsCount = "Command count: ${count++} "
+                            println("Command count sent to client: $sendsCount ")
                             output.println(sendsCount)
-                            val commandLine = fragmentedCmdslist[0]
+                            val commandLine = fragmentedCmdslist[Random.nextInt(0, 14)]
                             val commands = commandLine.split(";")
                             for (cmd in commands) {
                                 if (output != null) {
-                                    println("Comando frammentato inviato al client:$cmd")
+                                    println("Comando frammentato inviato al client:$cmd ")
                                     output.println(cmd)
                                 }
                                 // Simula una pausa tra l'invio di ciascun frammento
-                                delay(500)
+                                delay(Random.nextInt(200, 2000).toLong())
                             }
                         }
                     } catch (e: Exception) {
@@ -227,76 +218,6 @@ class DSCApplication : MultiDexApplication() {
 
 
         }
-
-
-        // Versione corretta di handleClient che riceve il comando dal socket
-        suspend fun handleClient(clientSocket: Socket, command: String) {
-            withContext(Dispatchers.IO) {
-                clientSocket.use { socket ->
-                    // val input = socket.getInputStream().bufferedReader()
-                    val output = socket.getOutputStream()?.let { PrintWriter(it, true) }
-
-                    // Leggi il comando inviato dal client
-                    /*val command = input.readLine()
-                println("Comando intero inviato dal client :${command}")*/
-
-                    // Suddividi il comando in token basati sul separatore e inviali al client
-                    val commands = command.split(";")
-                    println("Parametro comando arrivato : $commands")
-                    if (commands.isNullOrEmpty()) {
-                        if (output != null) {
-                            output.println(command)
-                        }
-                        println("Comando intero inviato al client: $command")
-                    } else {
-                        println("Comando frammentato inviato al client:")
-                        for (cmd in commands) {
-                            if (output != null) {
-                                output.println(cmd)
-                            }
-                            // Simula una pausa tra l'invio di ciascun frammento
-                            delay(500)
-                        }
-                    }
-
-
-                    // Simula l'invio di un comando frammentato
-                    /*val commands_01 = listOf(
-                    "<CMD id=\"example_01\">",
-                    "Parte1 del comando_01; ",
-                    "Parte2 del comando_01; ",
-                    "Parte3 del comando_01; ",
-                    "</CMD>"
-                )
-
-                for (command in commands_01) {
-                    output.println(command)
-                    // Simula una pausa tra l'invio di ciascun frammento
-                    // delay(100) // sostituisce Thread.sleep() in un contesto di coroutine
-                }
-
-                // Simula l'invio di un comando frammentato
-                val commands_02 = listOf(
-                    "<CMD id=\"example_02\">",
-                    "Parte1 del comando_02; ",
-                    "Parte2 del comando_02; ",
-                    "Parte3 del comando_02; ",
-                    "</CMD>"
-                )
-
-                for (command in commands_02) {
-                    output.println(command)
-                    // Simula una pausa tra l'invio di ciascun frammento
-                    // delay(100) // sostituisce Thread.sleep() in un contesto di coroutine
-                }*/
-
-                    // Chiudi la connessione
-                    // socket.close()
-
-                }
-            }
-        }
-
 
 
 
--- app/src/main/java/com/dinema/dsc/dscserversocketsim/MainActivity.kt
+++ app/src/main/java/com/dinema/dsc/dscserversocketsim/MainActivity.kt
@@ -16,27 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.button_01).setOnClickListener { sendCommand("comando1;Parte1;Parte2;Parte3") }
-        findViewById<Button>(R.id.button_02).setOnClickListener { sendCommand("comando2;ParteA;ParteB;ParteC") }
+        /*findViewById<Button>(R.id.button_01).setOnClickListener { sendCommand("comando1;Parte1;Parte2;Parte3") }
+        findViewById<Button>(R.id.button_02).setOnClickListener { sendCommand("comando2;ParteA;ParteB;ParteC") }*/
     }
 
-    /*private fun sendCommand(command: String) {
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val socket = Socket(serverAddress, serverPort)
-                val output = PrintWriter(socket.getOutputStream(), true)
-                output.println(command)
-                socket.close()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }*/
 
-    private  fun sendCommand(command: String) {
+
+    /*private  fun sendCommand(command: String) {
         CoroutineScope(Dispatchers.IO).launch {
             DSCApplication.clientSocket?.let { DSCApplication.handleClient(it,command) }
         }
-    }
+    }*/
 }
